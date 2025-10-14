3023a5c63fe536e5bc49bd9c3d4ba0341af88b98
Lgsus23 <jesusbernardops23@gmail.com>
2025-10-14 10:30:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+ 
+  const sujetos = ["mi hamster", "mi novia", "el socialismo", "un boliviano"];
+  const acciones = ["se comió", "fulminó", "traumatizó", "profanó"];
+  const objetos = ["la economía", "mi dignidad", "a la clase obrera", "un radiador"];
+  const tiempos = ["antes de darse cuenta", "después de su descanso", "en Perú", "después de desayunar", "porque se sentía infeliz"];
+
+  let sujeto = Math.floor(Math.random() * sujetos.length);
+  let accion = Math.floor(Math.random() * acciones.length);
+  let objeto = Math.floor(Math.random() * objetos.length);
+  let tiempo = Math.floor(Math.random() * tiempos.length);
+
+  let palabraSujeto = sujetos[sujeto];
+  let palabraAccion = acciones[accion];
+  let palabraObjeto = objetos[objeto];
+  let palabraTiempo = tiempos[tiempo];
+
+  let excusa = palabraSujeto + " " + palabraAccion + " " + palabraObjeto + " " + palabraTiempo + ".";
+
+  
+  let elementoExcusa = document.getElementById("excusa");
+  elementoExcusa.innerText = excusa;
 };
